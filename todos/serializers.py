3e3d1d84a32ba3506b22bcfe68ea2ea1a2c1431c
jw <betrayers000@gmail.com>
2019-11-19 14:10:25 +0900

    todo list

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Todo
+from .models import Todo, User
 
+# todo 하나를 조작하기 위한 serializer
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'user', 'title', 'completed',)
+
+# todo_list 
+class UserSerializer(serializers.ModelSerializer):
+    todo_set = TodoSerializer(many=True)
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'todo_set')
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Todo
-from .serializers import TodoSerializer
+from .models import Todo, User
+from .serializers import TodoSerializer, UserSerializer
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from django.http import JsonResponse, HttpResponse
 from rest_framework.permissions import IsAuthenticated
@@ -31,7 +31,19 @@ def todo_detail(request, id):
         if serialiser.is_valid():
             serialiser.save()
             return JsonResponse(serialiser.data)
+        return HttpResponse(status=400)
     elif request.method == "DELETE":
         todo.delete()
         return JsonResponse({'msg': 'delete'})
-    return HttpResponse(status=400)
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+@authentication_classes((JSONWebTokenAuthentication, ))
+def user_detail(request, id):
+    user = get_object_or_404(User, id=id)
+    if request.user != user:
+        return HttpResponse(status=404)
+    if request.method == "GET":
+        serializer = UserSerializer(user)
+        return JsonResponse(serializer.data)
