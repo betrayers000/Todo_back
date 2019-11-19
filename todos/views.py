91b2b371170f78de420696d9620be7d2137027fe
jw <betrayers000@gmail.com>
2019-11-19 13:48:27 +0900

    todo detail - get, put, delete

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Todo
 from .serializers import TodoSerializer
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from django.http import JsonResponse, HttpResponse
@@ -15,4 +16,22 @@ def todo_create(request):
         serializer.save()
         return JsonResponse(serializer.data)
     return HttpResponse(status=400)
-    
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated, ))
+@authentication_classes((JSONWebTokenAuthentication, ))
+def todo_detail(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == "GET":
+        serialiser = TodoSerializer(todo)
+        return JsonResponse(serialiser.data)
+    elif request.method == "PUT":
+        serialiser = TodoSerializer(todo, data=request.data)
+        if serialiser.is_valid():
+            serialiser.save()
+            return JsonResponse(serialiser.data)
+    elif request.method == "DELETE":
+        todo.delete()
+        return JsonResponse({'msg': 'delete'})
+    return HttpResponse(status=400)
